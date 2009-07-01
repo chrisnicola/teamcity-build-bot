06e8ee848b7330cf697a04be0bc616b62b8f3295
Adam Pearson <ampearson@gmail.com>
2009-07-01 10:22:48 -0700

    1. Added BuildRepo class which is responsible for maintaining a FIFO queue of builds and reporting whether the latest one failed
    2. Persist repo hash on quit, load on start
    
    3. added history and status commands

---
+++ lib/build_repo.rb
@@ -0,0 +1,31 @@
+class BuildRepo
+  attr_accessor :name, :limit, :builds
+  def initialize(name)
+    @name = name
+    @limit = 30
+    @builds = []
+  end
+  
+  def <<(item)
+    @builds << item
+    if @builds.size > @limit
+      puts "I'm shifting"
+      @builds.shift
+    end
+    
+    @builds.sort! {|a,b| a.number <=> b.number}
+  end
+
+  def include?(item)
+    o = @builds.detect{|b| b.number == item.number}
+    !o.nil?
+  end
+  
+  def latest_build
+    @builds.last
+  end
+  
+  def currently_failed?
+    latest_build.failed
+  end
+end       
--- lib/buildbot.rb
+++ lib/buildbot.rb
@@ -1,5 +1,6 @@
 require 'smirc'
 require File.join(File.dirname(__FILE__),'build_listener')
+require File.join(File.dirname(__FILE__),'build_repo')
 
 class BuildBot
   def initialize(configuration)
@@ -8,13 +9,16 @@ class BuildBot
     @irc = Smirc::Client.new(configuration)
     @threads = []
     @items = {}
+    @repos = {}
     @verbose = @configuration.verbose  
+    @has_reported = false
   end
 
   def run
+    load_data
     Thread.abort_on_exception = true
     @listener.on(:log) {|l, m| print "LIS #{m}\n"}
-    @listener.on(:item) {|l, i| process_item(i)}
+    @listener.on(:item) {|l, i| @has_reported = true; process_item(i)}
     @irc.on(:connected) do |irc|
       irc.join(@configuration.channel)
       @threads << Thread.new do
@@ -41,11 +45,27 @@ class BuildBot
 
   def parse_message(irc, channel, msg, usr)
     case msg
-    when Regexp.new("Hi|hi(.*)#{@configuration.nickname}")
-      irc.message(channel, "Hi there, #{usr}")
-    when Regexp.new("#{@configuration.nickname}(.*)status")
+#    when Regexp.new("Hi|hi(.*)#{@configuration.nickname}")
+      #irc.message(channel, "Hi there, #{usr}")
+    when Regexp.new("#{@configuration.nickname}(.*) config status")
       irc.message(channel, "I am running")
       irc.message(channel, "Verbose is: #{@verbose}")
+
+    when Regexp.new("#{@configuration.nickname}(.*) build status")
+      irc.message(channel, "Known builds:")
+      @repos.keys.each do |k|
+        irc.message(channel, "#{k}: #{@repos[k].latest_build.failed ? 'failed' : 'succeeded'}")
+      end
+    when /status of (.*)/
+      if @repos.has_key?($1)
+        report_build(@repos[$1].latest_build, false)
+      end
+    when /history of (.*)/
+      if @repos.has_key?($1)
+        @repos[$1].builds.reverse[0..10].each do |b|
+          report_build(b, false)
+        end
+      end
     when /life(.*)universe(.*)everything/
       irc.message(channel, "42")
     when /prom/
@@ -59,22 +79,51 @@ class BuildBot
 
   def process_item(item)
 
-    return if @items.has_key?(item.guid)
-    
-    @items[item.guid] = item
-    if item.failed || @verbose
-      report_build(item)
+    unless @repos.has_key?(item.build)
+      @repos[item.build] = BuildRepo.new(item.build)
     end
+    build = @repos[item.build]
+    unless build.include?(item)
+      #print "build #{build.name} did not include #{item.title}\n"
+      #build.builds.each do |b|
+        #print b.title + "\n"
+      #end
+
+      if item.failed || @verbose
+        report_build(item)
+      elsif build.currently_failed? && !item.failed
+        report_build(item, false)
+      end
+      build << item 
+    end
+
+    
   end
 
-  def report_build(item)
+  def report_build(item, include_link = true)
     @irc.message(@configuration.channel, "#{item.build} #{item.number} #{item.failed ? 'failed' : 'succeeded'}")
-    @irc.message(@configuration.channel, "#{item.link}")
+    if include_link
+      @irc.message(@configuration.channel, "#{item.link}")
+    end
   end
     
 
   def stop
+    save_data if @has_reported
     @irc.disconnect
+  end
+
+  def load_data
+    if File.exist?('saved_data')
+      @repos = Marshal.load(File.open('saved_data'))
+    end
+  end
+
+  def save_data
+    puts "SAVING DATA"
+    File.open("saved_data", "w+") do |f|
+      Marshal.dump(@repos, f)
+    end
   end
 end
 
